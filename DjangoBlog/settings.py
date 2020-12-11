af3989b399ac7cca9aca7ddebc6f64a546fc0a4c
Çlirim Furriku <clirimfurriku@users.noreply.github.com>
2020-12-11 11:13:12 +0100

    Update admin templates for password change/reset

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'DjangoBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +129,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class MyUserManager(BaseUserManager):
     def create_user(self, email, username, user_type, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
         if not email:
             raise ValueError('Users must have an email address')
         elif not username:
@@ -26,10 +22,6 @@ class MyUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, username, user_type, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
         user = self.create_user(
             email,
             password=password,
