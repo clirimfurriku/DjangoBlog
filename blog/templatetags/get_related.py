2ed2ce482c310bd148f6063017c58c0eed217247
Çlirim Furriku <clirimfurriku@users.noreply.github.com>
2021-01-19 20:23:23 +0100

    Added similar posts recommendations to post_detail

---
+++ blog/templatetags/get_related.py
@@ -0,0 +1,11 @@
+from django import template
+
+from blog.models import BlogPost, UserComment
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_similar_posts(post: BlogPost) -> BlogPost:
+    """Get a list of post sorted based of likes on the same category where the post is"""
+    return BlogPost.objects.filter(category=post.category.first()).exclude(id=post.id).order_by('stars')[:4]
