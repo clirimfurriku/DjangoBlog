f36019eaab09519dc85b9fdc04731151805f4b83
Çlirim Furriku <clirimfurriku@users.noreply.github.com>
2021-02-19 12:00:13 +0100

    Limit related posts to 3

--- blog/templatetags/get_related.py
+++ blog/templatetags/get_related.py
@@ -8,4 +8,4 @@ register = template.Library()
 @register.simple_tag
 def get_similar_posts(post: BlogPost) -> BlogPost:
     """Get a list of post sorted based of likes on the same category where the post is"""
-    return BlogPost.objects.filter(category=post.category.first(), banned=False).exclude(id=post.id).order_by('likes')[:4]
+    return BlogPost.objects.filter(category=post.category.first(), banned=False).exclude(id=post.id).order_by('likes')[:3]
