f171270addc3f106146c6f7e672ab9532a3ff3d4
Çlirim Furriku <clirimfurriku@users.noreply.github.com>
2021-02-19 12:27:50 +0100

    Properly filter related posts

--- blog/templatetags/get_related.py
+++ blog/templatetags/get_related.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from blog.models import BlogPost, UserComment
 
@@ -8,4 +9,5 @@ register = template.Library()
 @register.simple_tag
 def get_similar_posts(post: BlogPost) -> BlogPost:
     """Get a list of post sorted based of likes on the same category where the post is"""
-    return BlogPost.objects.filter(category=post.category.first(), banned=False).exclude(id=post.id).order_by('likes')[:3]
+    return BlogPost.objects.filter(category=post.category.first(), banned=False).exclude(id=post.id) \
+               .annotate(count=Count('likes')).order_by('-count').prefetch_related("category", "author")[:3]
